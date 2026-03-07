bb816400c91bb1a90d0caaa30b7a8cc0e4b3aa2e
BadBoy-Github <elayabarathiedison@gmail.com>
2026-03-08 00:55:35 +0530

    contact form email updated

--- api/email.js
+++ api/email.js
@@ -49,7 +49,7 @@ module.exports = async function handler(req, res) {
                 }
 
                 const mailOptions = {
-                    from: fromEmail,
+                    from: "Card Vault",
                     to: process.env.DEFAULT_ADMIN_EMAIL || 'elayabarathiedison@gmail.com',
                     subject: `[${category}] ${subject} - Contact Form`,
                     html: `
