7a73afbffb9908d28f1de18422156eebbf4fc00a
BadBoy-Github <elayabarathiedison@gmail.com>
2026-04-29 09:36:26 +0530

    Fix reminders API to accept GET requests from Vercel cron

--- api/reminders.js
+++ api/reminders.js
@@ -236,7 +236,7 @@ module.exports = async function handler(req, res) {
         return res.status(204).end();
     }
 
-    if (req.method !== 'POST') {
+    if (req.method !== 'POST' && req.method !== 'GET') {
         return res.status(405).json({ message: 'Method not allowed' });
     }
 
